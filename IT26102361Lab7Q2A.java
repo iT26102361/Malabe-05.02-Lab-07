557c2def36f4025729f7c21667d4e77432f0b553
iT26102361 <it26102361@my.sliit.lk>
2026-09-16 10:02:48 +0530

    Delete IT26102361Lab7Q2A.java

--- IT26102361Lab7Q2A.java
+++
@@ -1,8 +0,0 @@
-public class IT26102361Lab7Q2A {
-    public static void main(String[] args) {
-
-        for (int i = 1; i <= 20; i++) {
-            System.out.println("$$$$ ");
-        }
-    }
-}
