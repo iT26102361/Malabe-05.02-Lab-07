925761bc4cf4d84b120d3fd0c8ceebf17c4595c5
iT26102361 <it26102361@my.sliit.lk>
2026-09-16 10:03:31 +0530

    Add files via upload

---
+++ IT26102361Lab7Q2A.java
@@ -0,0 +1,8 @@
+public class IT26102361Lab7Q2A {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 4; i++) {
+            System.out.println("$ $ $ $ $");
+        }
+    }
+}
